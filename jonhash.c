8674d83deb551c123c7db6ba56b21ec06398c277
Jonathan Marsh <j.r.marsh@ntlworld.com>
2018-07-21 23:40:29 +0100

    2/3 of conversion complete

--- jonhash.c
+++ jonhash.c
@@ -9,20 +9,20 @@
 #include <stdio.h> //TODO: remove
 
 /*Compares 2 strings of binary and XORs them.
-	output:	char* binary[BLOCK_SIZE * CHAR_BIT];
-	input1:	char* binary[BLOCK_SIZE * CHAR_BIT];
-	input2:	char* binary[BLOCK_SIZE * CHAR_BIT];
+	output:	char XORed[BLOCK_SIZE * CHAR_BIT];
+	input1:	char currentBinary[BLOCK_SIZE * CHAR_BIT];
+	input2:	char shiftedBinary[BLOCK_SIZE * CHAR_BIT];
 */
 void XOR(char* output, char* input1, char* input2) {
 	for (unsigned int index = 0; index < BLOCK_SIZE * CHAR_BIT; index++)
-		output[index] = (input1[index] != input2[index]) ? '1' : '0';
+		output[index] = (input1[index] != input2[index]) ? 1 : 0;
 }
 
 /*Left-shifts binary string `shift` times
-	output:	char* binary[BLOCK_SIZE * CHAR_BIT];
-	input:	char* binary[BLOCK_SIZE * CHAR_BIT];
-	shift:	unsigned int;
-*/
+	output:	char shifted[BLOCK_SIZE * CHAR_BIT];
+	input:	char currentBinary[BLOCK_SIZE * CHAR_BIT];
+	shift:	unsigned int shiftBy;
+*/ 
 void shift(char* output, char* input, unsigned int shift) {
 	for (unsigned int index = 0; index < BLOCK_SIZE * CHAR_BIT; index++)
 		if(index < shift)
@@ -32,15 +32,16 @@ void shift(char* output, char* input, unsigned int shift) {
 }
 
 /*Converts a base64 string to concatenated binary string
-	output:	char* binary[BLOCK_SIZE * CHAR_BIT];
-	input: 	char* binary[BLOCK_SIZE];
+	output:	char binary[BLOCK_SIZE * CHAR_BIT];
+	input: 	char base64[BLOCK_SIZE];
 */
 void convertBase64ToBinary(char* output, char* input) {
-	for (unsigned int index = 0; index < BLOCK_SIZE; index++){
-		unsigned int quotient = (unsigned int)input[index],
-			base = CHAR_BIT;
+	unsigned int index = 0, quotient, base;
+	for (; index < BLOCK_SIZE; index++){
+		quotient = (unsigned int)input[index];
+		base = CHAR_BIT;
 		while(quotient != 0 || base > 0){
-			output[index * CHAR_BIT + base] = (quotient != quotient / 2 * 2) ? '1' : '0';
+			output[index * CHAR_BIT + base] = (quotient != quotient / 2 * 2) ? 1 : 0;
 			quotient /= 2;
 			base--;
 		}
@@ -48,18 +49,21 @@ void convertBase64ToBinary(char* output, char* input) {
 }
 
 /*Converts concatenated binary string to a base64 string
-	output:	char* binary[BLOCK_SIZE];
-	input: 	char* binary[BLOCK_SIZE * CHAR_BIT];
+	output:	char base64[BLOCK_SIZE];
+	input: 	char binary[BLOCK_SIZE * CHAR_BIT];
 */
 void convertBinaryToBase64(char* output, char* input) {
-	unsigned int base = 2, total = 0, increment = 0;
-	for (unsigned int index = BLOCK_SIZE * CHAR_BIT - 1; index >= 0; index--){
+	unsigned int base = 2, 
+				total = 0, 
+				increment = 0,
+				index = BLOCK_SIZE * CHAR_BIT - 1;
+	for (; index >= 0; index--){
 		total += input[index] * base;
 		base *= 2;
 		increment++;
 		if(increment == CHAR_BIT){
 			base = 2;
-			output[index / CHAR_BIT] = total; 
+			output[index / CHAR_BIT] = BASE64[total]; 
 			total = 0;
 			increment = 0;
 		}
@@ -71,72 +75,72 @@ void convertBinaryToBase64(char* output, char* input) {
 	-Encrypts by shifting 
 	-XORs the encrypted binary with the previous binary
 */
-char* hash(char* currentBlock, char* previousHash) {
-	char* currentBinary = convertToBinary(currentBlock),
-		  previousBinary = convertToBinary(previousHash);
+void hash(char* output, char* input) {
+	char currentBinary[BLOCK_SIZE * CHAR_BIT] = {0},
+		previousBinary[BLOCK_SIZE * CHAR_BIT] = {0},
+		shifted[BLOCK_SIZE * CHAR_BIT] = {0},
+		XORed[BLOCK_SIZE * CHAR_BIT] = {0};
 
-	char shifted[BLOCK_SIZE * CHAR_BIT];
-	size_t shiftBy = 1;
-	for (char character : previousBinary)
-		if (character == '1')
+	convertBase64ToBinary(currentBinary, input),
+	convertBase64ToBinary(previousBinary, output);
+
+	unsigned int shiftBy = 1, index = 0;
+	for (; index < BLOCK_SIZE * CHAR_BIT; index++)
+		if (previousBinary[index] == 1)
 			shiftBy++;
-	for (size_t i = 0; i < 1; i++) 
-		shift(shifted, currentBinary, shiftBy);
 
-	char XORed[BLOCK_SIZE * CHAR_BIT];
-	XOR(XORed, currentBinary, previousBinary)
-	return convertFromBinary(XORed);
+	shift(shifted, currentBinary, shiftBy);
+
+	XOR(XORed, currentBinary, previousBinary);
+	convertBinaryToBase64(output, XORed);
 }
 
-/*Appends a 1 then appends 0s to make it a BLOCK_SIZE multiple*/
-void padAlign(char* input) {
-	input += '1';
-	size_t padBy = 2 * BLOCK_SIZE - input.size() % BLOCK_SIZE;
-	for (size_t i = 0; i < padBy; i++) 
-		input += '0';
-}
-
-/*Alternates between the password and salt (weaves)*/
-void weave(char* password, char* salt) {
-	char* woven;
-	for (size_t i = 0; i < password.size(); i++) {
-		woven += password[i];
-		if (i < salt.size()) 
-			woven += salt[i]; //Add next salt character if there's any left
-	}
-	if (password.size() < salt.size()) //Will be left over salt, append it all
-		woven += salt.substr(password.size());
-	password = woven;
+unsigned int random(){
+	return 1; /*TODO: CSPRNG*/
 }
 
 /*Creates a random salt using the new random function*/
-char* createSalt() {
-	std::string salt = "";
-	for (size_t i = 0; i < BLOCK_SIZE; i++)
-		salt.push_back(BASE64[randomCharacter(random)]);
-	return salt;
+void createSalt(char* output) {
+	for (unsigned int index = 0; index < BLOCK_SIZE; index++)
+		output[index] = BASE64[random()]; 
 }
 
 /*Creates the final hash by compressing the output of each stage*/
-char* hashPassword(char* password, char* salt = createSalt()) {
-	weave(password, salt);
-	padAlign(password);
-	char* hashed = H0;
-	for (size_t iterate = 0; iterate < COST * 1000; iterate++)
-		for (size_t i = 0; i < password.size() / BLOCK_SIZE; i++) {
-			char* result = hash(password.substr(i * BLOCK_SIZE, BLOCK_SIZE), hashed);
-			hashed = result;
+void hashPassword(char* output, char* input) {
+	createSalt(output);
+	char hashed[BLOCK_SIZE] = H0;
+	unsigned int iteration = 0, index;
+	for (; iteration < COST * 1000; iteration++){
+		index = 0;
+		while(input[index] != '\0'){
+			char block[BLOCK_SIZE] = {0};
+			
+			hash(hashed, block);
 		}
-	return salt + hashed;
+	}
+	for(index = 0; index < BLOCK_SIZE; index++)
+		output[BLOCK_SIZE + index] = hashed[index];
+}
+
+int testBaseConversions(){
+    char input[BLOCK_SIZE] = "jonathanmarsh123jonathanmarsh123";
+    char binary[BLOCK_SIZE * CHAR_BIT + 1];
+    convertBase64ToBinary(binary, input);
+	binary[BLOCK_SIZE * CHAR_BIT] = '\0';
+    printf("Binary: %s\n", binary);
+    
+    char base64[BLOCK_SIZE + 1];
+    convertBinaryToBase64(base64, binary);
+	base64[BLOCK_SIZE] = '\0';
+    printf("Base64: %s\n", base64);
+
+	return base64 == input;
 }
 
 
 int main(int argc, char* argv[]){
-	printf("%d",argc);
-	if(argc == 1)  hashPassword(getPassword());
-
-	char* savedHash = hashPassword(getPassword());
-	char* hashed = hashPassword(getPassword(), savedHash.substr(0, BLOCK_SIZE));
-	if (hashed == savedHash) return 0;
-	else return 1;
+	printf("Number of arguments: %d\n",argc);
+	printf("Conversion test: %d\n",testBaseConversions());
+	
+	return 0;
 }
